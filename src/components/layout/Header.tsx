4f5fd99e67ee53f83093934cbb70e5f81047aef8
rezafiansyah <rezafiansyah.putra@gmail.com>
2021-04-03 11:50:51 +0700

    Styling font size for responsive

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -7,7 +7,12 @@ const Header = () => {
   return (
     <Flex as="header" width="full" align="center">
       <AccessibleLink href="/">
-        <Heading fontSize="50px" fontFamily="Baskerville">Dapatkan Sertifikat Kimia Farma Sehat Dengan Berbagi Berkah</Heading>
+        <Heading
+          fontSize={{ base: "24px", md: "36px", lg: "48px" }}
+          fontFamily="Baskerville"
+        >
+          Dapatkan Sertifikat Kimia Farma Sehat Dengan Berbagi Berkah
+        </Heading>
       </AccessibleLink>
 
       <Box marginLeft="auto">
